5a91812d8f8e664acce2e72a2183860069e3a933
Ayyu Meta Paramitha <metamitha123@gmail.com>
2025-06-03 13:24:32 +0700

    Update timeline.php

--- modul-5/timeline.php
+++ modul-5/timeline.php
@@ -10,15 +10,8 @@
   <div class="timeline">
     <?php
     $pengalaman = [
-<<<<<<< HEAD
       ["tahun" => "Semester 1", "deskripsi" => "Masuk kuliah dan mulai belajar dasar pemrograman Python"],
       ["tahun" => "Semester 2", "deskripsi" => "Belajar HTML,CSS,JavaScript,PHP"],
-=======
-      ["tahun" => "2021", "deskripsi" => "Masuk kuliah dan mulai belajar dasar pemrograman"],
-      ["tahun" => "2022", "deskripsi" => "Membuat proyek pertama dengan HTML/CSS"],
-      ["tahun" => "2023", "deskripsi" => "Belajar PHP dan membuat aplikasi dinamis"],
-      ["tahun" => "2024", "deskripsi" => "Magang di perusahaan teknologi"],
->>>>>>> 8c9d936ad2161bcdab20491126d16b792b75ab16
     ];
 
     foreach ($pengalaman as $item) {
